caf71eb35ef0c9ca5e980df0f847f824e205e0f6
dk7648 <dk7648@korea.ac.kr>
2026-03-25 20:42:33 +0900

    feat: 시간 측정 로직 추가

--- pipeline/services/crawl_orchestrator.py
+++ pipeline/services/crawl_orchestrator.py
@@ -1,3 +1,7 @@
+import logging
+import time
+
+logger = logging.getLogger(__name__)
 class CrawlOrchestrator:
     def __init__(self, request_generator, crawl_service, csv_service, tag_service, transfer_service):
         self.request_generator = request_generator
@@ -7,12 +11,29 @@ class CrawlOrchestrator:
         self.transfer_service = transfer_service
 
     async def run(self):
-        request = self.request_generator.generate()
-        response = await self.crawl_service.collect(request.targets)
-        crawled_file_paths = self.csv_service.write_stock_csv(response.stocks)
-        tagged_file_paths = self.tag_service.tag_csv(crawled_file_paths)
-        self.transfer_service.transfer_all(tagged_file_paths)
+        total_start = time.perf_counter()
 
+        t = time.perf_counter()
+        request = self.request_generator.generate()
+        logger.info("request 생성 elapsed=%.3fs", time.perf_counter() - t)
+
+        t = time.perf_counter()
+        response = await self.crawl_service.collect(request.targets)
+        logger.info("crawl elapsed=%.3fs", time.perf_counter() - t)
+
+        t = time.perf_counter()
+        crawled_file_paths = self.csv_service.write_stock_csv(response.stocks)
+        logger.info("csv 저장 elapsed=%.3fs", time.perf_counter() - t)
+
+        t = time.perf_counter()
+        tagged_file_paths = self.tag_service.tag_csv(crawled_file_paths)
+        logger.info("tag elapsed=%.3fs", time.perf_counter() - t)
+
+        t = time.perf_counter()
+        self.transfer_service.transfer_all(tagged_file_paths)
+        logger.info("db 적재 elapsed=%.3fs", time.perf_counter() - t)
+
+        logger.info("pipeline total elapsed=%.3fs", time.perf_counter() - total_start)
         return {
             "count": response.count,
             "stocks": response.stocks,
