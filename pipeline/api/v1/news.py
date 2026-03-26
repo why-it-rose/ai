4b2d0e2be71244348d3f15a4e9f9b6ef45aacd38
dk7648 <dk7648@korea.ac.kr>
2026-03-26 10:43:43 +0900

    feat(api): 뉴스 크롤링 로직별 endpoint url 분리

--- pipeline/api/v1/news.py
+++ pipeline/api/v1/news.py
@@ -12,10 +12,19 @@ async def crawl_news(background_tasks: BackgroundTasks):
     background_tasks.add_task(run_crawl_job)
     return {"message": "작업이 등록됐습니다."}
 
+@router.get("/crawl-today")
+async def crawl_news(background_tasks: BackgroundTasks):
+    background_tasks.add_task(run_crawl_job, True)
+    return {"message": "오늘의 뉴스 작업이 등록됐습니다."}
 
-async def run_crawl_job():
+async def run_crawl_job(is_today: bool = False):
     try:
-        await crawl_orchestrator.run()
+        if(is_today):
+            logger.info("오늘의 뉴스 크롤링 작업 시작")
+            await crawl_orchestrator.run_today()
+        else:
+            logger.info("전체 크롤링 작업 시작")
+            await crawl_orchestrator.run_entire()
         logger.info("크롤링 작업 완료")
     except Exception as e:
         logger.exception("크롤링 작업 실패: %s", e)
